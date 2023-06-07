e8d1cabd85d70f72911375160460a10d0254e7bb
Ethan Feldman <ejfeldman7@gmail.com>
2023-06-06 23:35:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -138,8 +138,8 @@ elif choice == 'Filter':
 
 elif choice == 'Recommender':
 	df = pd.read_parquet('./data/hikes.parquet')
-	st.markdown("## Get a Hike Recommendation - Similar Descriptions")
-	st.markdown("## **Not Using Hike Stats Yet (length, elevation, etc)")
+	st.markdown("# Get a Hike Recommendation - Similar Descriptions")
+	st.markdown("####**Not Using Hike Stats Yet (length, elevation, etc)**")
 	st.markdown("##### First, type in the name of a hike and hit enter")
 	st.markdown("##### Then, select the hike from the drop down")
 
