941fe5d7f392863a194f100329091a47b37df929
Ethan Feldman <ejfeldman7@gmail.com>
2023-06-09 00:29:05 -0700

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 
+@st.cache_data
 def load_data(filepath, file_type):
-	@st.cache_data
 	if file_type == 'parquet':
 		df = pd.read_parquet(filepath)
 	elif file_type == 'csv':
