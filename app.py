4fecb932a4baf112f449b407d3457953affd2a1c
Ethan Feldman <ejfeldman7@gmail.com>
2023-06-09 00:39:02 -0700

    Add openai

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from streamlit_chat import message
 import os
 import pickle
 import sklearn
@@ -12,6 +13,7 @@ from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
+import openai
 
 from utils.processing import scaler
 from utils.feature_lists import object_cols, numeric_cols, tag_cols
@@ -311,3 +313,40 @@ elif choice == 'Recommender':
 			st.write('Or, another option I would recommend you try is:',text_df.iloc[recs[1]]['Name'],'\n\n','It could be desribed as:','\n',text_df.iloc[recs[1]].Description)
 			st.write('For more information, visit:',text_df.iloc[recs[1]]['Hike ID'],'\n')
 			st.write('Other choices include:', text_df.iloc[recs[2]]['Hike ID'],', ',text_df.iloc[recs[3]]['Hike ID'],', ',text_df.iloc[recs[4]]['Hike ID'])
+
+elif choice == 'Chat HikeGPT':
+
+
+with st.sidebar:
+    openai_api_key = st.text_input('OpenAI API Key',key='chatbot_api_key')
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+	st.title("💬 Streamlit GPT")
+	#openai.api_key = st.secrets.openai_api_key
+	if "messages" not in st.session_state:
+	    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+	with st.form("chat_input", clear_on_submit=True):
+	    a, b = st.columns([4, 1])
+	    user_input = a.text_input(
+	        label="Your message:",
+	        placeholder="Ask me about finding a hike!",
+	        label_visibility="collapsed",
+	    )
+	    b.form_submit_button("Send", use_container_width=True)
+
+	for msg in st.session_state.messages:
+	    message(msg["content"], is_user=msg["role"] == "user")
+
+	if user_input and not openai_api_key:
+	    st.info("Please add your OpenAI API key to continue.")
+	    
+	if user_input and openai_api_key:
+	    openai.api_key = openai_api_key
+	    st.session_state.messages.append({"role": "user", "content": user_input})
+	    message(user_input, is_user=True)
+	    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+	    msg = response.choices[0].message
+	    st.session_state.messages.append(msg)
+	    message(msg.content)
