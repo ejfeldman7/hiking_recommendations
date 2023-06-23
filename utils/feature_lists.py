03fa9a6699591355d6838f0f2e7db0d809b23d47
Ethan Feldman <ejfeldman7@gmail.com>
2023-06-23 09:51:38 -0700

    Update feature cols

--- utils/feature_lists.py
+++ utils/feature_lists.py
@@ -6,6 +6,6 @@ fewer_numeric = ['Length', 'Elevation Gain']
 
 object_cols = ['General Region', 'Specific Region', 'One Way', 'Round Trip']
 
-fewer_object = ['One Way', 'Round Trip']
+fewer_object = ['One Way', 'Round Trip','General Region']
 
 tag_cols = ['Tags']
